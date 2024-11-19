adf15aa8f7d3e5955db562804d0a8821922fe0b3
QuincePie <flaky@quincepie.dev>
2024-11-18 23:59:30 -0600

    Add golang flake

---
+++ go/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A Nix Dev Env for Go";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs nixpkgs.lib.platforms.unix (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    formatter = forAllSystems ({pkgs}: pkgs.alejandra);
+    devShells = forAllSystems ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          go
+          golangci-lint
+          gopls
+        ];
+      };
+    });
+  };
+}
